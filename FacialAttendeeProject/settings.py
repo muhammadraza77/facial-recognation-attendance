8ec4c02f95003250cd2b1bdc82ff9ba56d88807f
muhammadraza77 <k163890@nu.edu.pk>
2019-05-06 23:39:22 +0500

    commit

--- FacialAttendeeProject/settings.py
+++ FacialAttendeeProject/settings.py
@@ -124,15 +124,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage
